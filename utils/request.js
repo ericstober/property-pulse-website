c53ff887b1cdbdf7e736a3a3a8198b70da5e68b0
ericstober <erictstober@gmail.com>
2024-07-15 16:12:50 -0500

    moved fetchProperties function to request.js

---
+++ utils/request.js
@@ -0,0 +1,15 @@
+const fetchProperties = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {fetchProperties};
