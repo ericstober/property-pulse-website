663a8f1d6090345ae58f088398fa2022bb6e262d
ericstober <erictstober@gmail.com>
2024-07-17 16:53:19 -0500

    add back from revert

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -14,3 +14,21 @@ export const GET = async (request) => {
     return new Response("Something went wrong", { status: 500 });
   }
 };
+
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData();
+
+    // Access all values from amenitites and images
+    const amenitites = formData.getAll('amenitites');
+    const images = formData.getAll('images').filter((image) => image.name !== '');
+
+    console.log(amenitites, images);
+
+    return new Response(JSON.stringify({ message: "Success" }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to add property", { status: 500 });
+  }
+};
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -25,14 +25,14 @@ export const authOptions = {
       // 2. Check if user exists
       const userExists = await User.findOne({ email: profile.email });
 
-      // 3. If user does not exist, then add use to database
+      // 3. If not, then add user to database
       if (!userExists) {
-        // Truncate username if too long
+        // Truncate user name if too long
         const username = profile.name.slice(0, 20);
 
         await User.create({
           email: profile.email,
-          username: username,
+          username,
           image: profile.picture,
         });
       }
