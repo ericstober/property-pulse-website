472952f146e44f588b5e882d80cf9f0fa7a64ef3
ericstober <erictstober@gmail.com>
2024-07-24 16:55:11 -0500

    added display search results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,6 +1,10 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
+"use client";
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams();
@@ -8,13 +12,15 @@ const SearchResultsPage = () => {
   const [properties, setProperties] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const location = searchParams.get('location');
-  const propertyType = searchParams.get('propertyType');
+  const location = searchParams.get("location");
+  const propertyType = searchParams.get("propertyType");
 
   useEffect(() => {
     const fetchSearchResults = async () => {
       try {
-        const res = await fetch(`/api/properties/search?location=${location}&propertyType=${propertyType}`);
+        const res = await fetch(
+          `/api/properties/search?location=${location}&propertyType=${propertyType}`
+        );
 
         if (res.status === 200) {
           const data = await res.json();
@@ -27,16 +33,32 @@ const SearchResultsPage = () => {
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     fetchSearchResults();
   }, [location, propertyType]);
 
-  console.log(properties);
-
-  return (
-    <div>SearchResultsPage</div>
-  )
-}
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        <Link href='/properties' className="flex items-center text-blue-500 hover:underline mb-3">
+          <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back To Properties
+        </Link>
+        <h1 className="text-2xl mb-4">Search Results</h1>
+        {properties.length === 0 ? (
+          <p>No search results found</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default SearchResultsPage;
