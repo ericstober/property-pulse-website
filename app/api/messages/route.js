9b4e6574a8a507a2331a2b94b9bd1d1213da96f6
ericstober <erictstober@gmail.com>
2024-07-25 15:43:19 -0500

    added messages page, messages component, and get api route for messages

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -4,12 +4,39 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
 
+// GET /api/messages
+export const GET = async () => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response(JSON.stringify("User ID is required"), {
+        status: 401,
+      });
+    }
+
+    const { userId } = sessionUser;
+
+    const messages = await Message.find({ recipient: userId })
+      .populate("sender", "name")
+      .populate("property", "title");
+
+    return new Response(JSON.stringify(messages), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
+
 // POST /api/messages
 export const POST = async (request) => {
   try {
     await connectDB;
 
-    const { name, email, phone, message, property, recipient } = await request.json();
+    const { name, email, phone, message, property, recipient } =
+      await request.json();
 
     const sessionUser = await getSessionUser();
 
