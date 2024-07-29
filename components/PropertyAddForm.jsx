6bc4aabb351324a9e6a63624d688a718f09cfa08
ericstober <erictstober@gmail.com>
2024-07-29 11:18:36 -0500

    cleaning up import

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const PropertyAddForm = () => {
   const [mounted, setMounted] = useState(false);
