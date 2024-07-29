809dcf92b7a8d8f3bbd328c22fbd047cbef863ce
ericstober <erictstober@gmail.com>
2024-07-29 11:27:03 -0500

    cleaning up imports

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
