eb9c80f09fa6cc31abf21cb44459fc5c1ffed7b5
ericstober <erictstober@gmail.com>
2024-07-29 11:24:21 -0500

    cleaning up imports

--- components/PropertyEditForm.jsx
+++ components/PropertyEditForm.jsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import { useParams, useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import { fetchProperty } from "@/utils/request";
